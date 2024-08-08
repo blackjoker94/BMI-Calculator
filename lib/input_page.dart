387930fea5ff89830803bc66afb89f5864f30b50
blackjoker94 <youssefeslam94@gmail.com>
2024-08-09 02:08:53 +0300

    added logic and project finished

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -2,16 +2,50 @@ import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'small_cards.dart';
 import 'gender_icon.dart';
+import 'result.dart';
+import 'calculator.dart';
 
 const bottomContainerH = 60.0;
 const cardsColor = Color(0xFF1D1E33);
+const inactiveCardsColor = Color(0xFF111328);
+const textstyleNUM = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
 
 class InputPage extends StatefulWidget {
   @override
   _InputPageState createState() => _InputPageState();
 }
 
+Color MaleColor = inactiveCardsColor;
+Color FemaleColor = inactiveCardsColor;
+bool isMaleActive = true;
+
+void updateColor(String gender) {
+  if (gender == "male" && isMaleActive) {
+    if (MaleColor == inactiveCardsColor) {
+      MaleColor = cardsColor;
+    } else {
+      MaleColor = inactiveCardsColor;
+      isMaleActive = false;
+    }
+  }
+  if (gender == "female" && !isMaleActive) {
+    if (FemaleColor == inactiveCardsColor) {
+      FemaleColor = cardsColor;
+      isMaleActive = false;
+    } else {
+      FemaleColor = inactiveCardsColor;
+      isMaleActive = true;
+    }
+  }
+}
+
 class _InputPageState extends State<InputPage> {
+  int height = 160;
+  int weight = 60;
+  int age = 25;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,25 +53,40 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
           Expanded(
             child: Row(
               children: [
                 Expanded(
-                  child: Smallcard(
-                    colour: cardsColor,
-                    cardChild: Gendericon(
-                      gendericon: FontAwesomeIcons.mars,
-                      gendertype: "MALE",
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor("male");
+                      });
+                    },
+                    child: Smallcard(
+                      colour: MaleColor,
+                      cardChild: Gendericon(
+                        gendericon: FontAwesomeIcons.mars,
+                        gendertype: "MALE",
+                      ),
                     ),
                   ),
                 ),
                 Expanded(
-                  child: Smallcard(
-                    colour: cardsColor,
-                    cardChild: Gendericon(
-                      gendericon: FontAwesomeIcons.venus,
-                      gendertype: "FEMALE",
+                  child: GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        updateColor("female");
+                      });
+                    },
+                    child: Smallcard(
+                      colour: FemaleColor,
+                      cardChild: Gendericon(
+                        gendericon: FontAwesomeIcons.venus,
+                        gendertype: "FEMALE",
+                      ),
                     ),
                   ),
                 ),
@@ -47,6 +96,58 @@ class _InputPageState extends State<InputPage> {
           Expanded(
             child: Smallcard(
               colour: cardsColor,
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "HEIGHT",
+                    style: TextStyle(
+                      fontSize: 18.0,
+                      color: Color(0xFF8D8E98),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: [
+                      Text(
+                        height.toString(),
+                        style: textstyleNUM,
+                      ),
+                      Text(
+                        "cm",
+                        style: TextStyle(
+                          fontSize: 18.0,
+                          color: Color(0xFF8D8E98),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SliderTheme(
+                    data: SliderTheme.of(context).copyWith(
+                      thumbShape:
+                          RoundSliderThumbShape(enabledThumbRadius: 15.0),
+                      overlayShape:
+                          RoundSliderOverlayShape(overlayRadius: 30.0),
+                      thumbColor: Color(0xFFEB1555),
+                      activeTrackColor: Colors.white,
+                      overlayColor: Color(0x29EB1555),
+                      inactiveTrackColor: Color(0xFF8D8E98),
+                    ),
+                    child: Slider(
+                      value: height.toDouble(),
+                      onChanged: (double newvalue) {
+                        setState(() {
+                          height = newvalue.round();
+                        });
+                      },
+                      min: 110.0,
+                      max: 220.0,
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
           Expanded(
@@ -55,24 +156,152 @@ class _InputPageState extends State<InputPage> {
                 Expanded(
                   child: Smallcard(
                     colour: cardsColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "WEIGHT",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                        Text(
+                          weight.toString(),
+                          style: textstyleNUM,
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RoundIconButton(
+                                  icon: FontAwesomeIcons.minus,
+                                  onPressed: () {
+                                    setState(() {
+                                      weight--;
+                                    });
+                                  }),
+                              SizedBox(width: 10.0),
+                              RoundIconButton(
+                                  icon: FontAwesomeIcons.plus,
+                                  onPressed: () {
+                                    setState(() {
+                                      weight++;
+                                    });
+                                  }),
+                            ]),
+                      ],
+                    ),
                   ),
                 ),
                 Expanded(
                   child: Smallcard(
                     colour: cardsColor,
+                    cardChild: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "AGE",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        ),
+                        Text(
+                          age.toString(),
+                          style: textstyleNUM,
+                        ),
+                        Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              RoundIconButton(
+                                  icon: FontAwesomeIcons.minus,
+                                  onPressed: () {
+                                    setState(() {
+                                      age--;
+                                    });
+                                  }),
+                              SizedBox(width: 10.0),
+                              RoundIconButton(
+                                  icon: FontAwesomeIcons.plus,
+                                  onPressed: () {
+                                    setState(() {
+                                      age++;
+                                    });
+                                  }),
+                            ]),
+                      ],
+                    ),
                   ),
                 ),
               ],
             ),
           ),
-          Container(
-            color: Color(0xFFEB1555),
-            margin: EdgeInsets.only(top: 10.0),
-            width: double.infinity,
-            height: bottomContainerH,
+          Calcbuttom(
+            text: "CALCULATE",
+            func: () {
+              calculator calc = calculator(height: height, weight: weight);
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => result(
+                            bmiResult: calc.CAlCbmi(),
+                            textResult: calc.getresult(),
+                            interResult: calc.getInterpretation(),
+                          )));
+            },
           ),
         ],
       ),
+    );
+  }
+}
+
+class Calcbuttom extends StatelessWidget {
+  const Calcbuttom({required this.text, required this.func});
+  final String text;
+  final VoidCallback func;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: func,
+      child: Container(
+        child: Center(
+            child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.bold,
+          ),
+        )),
+        color: Color(0xFFEB1555),
+        margin: EdgeInsets.only(top: 10.0),
+        width: double.infinity,
+        height: bottomContainerH,
+        padding: EdgeInsets.only(bottom: 15.0),
+      ),
+    );
+  }
+}
+
+class RoundIconButton extends StatelessWidget {
+  RoundIconButton({required this.icon, required this.onPressed});
+  final IconData? icon;
+  final VoidCallback onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return RawMaterialButton(
+      child: Icon(icon),
+      onPressed: onPressed,
+      constraints: BoxConstraints.tightFor(
+        width: 56.0,
+        height: 56.0,
+      ),
+      shape: CircleBorder(),
+      fillColor: Color(0xff4c4f5e),
+      elevation: 6.0,
+      disabledElevation: 6.0,
     );
   }
 }
---
+++ lib/result.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:quiz/input_page.dart';
+import 'package:quiz/small_cards.dart';
+
+class result extends StatelessWidget {
+  result(
+      {required this.bmiResult,
+      required this.textResult,
+      required this.interResult});
+  final String bmiResult;
+  final String textResult;
+  final String interResult;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("BMI CALCULATOR"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              padding: EdgeInsets.all(15.0),
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                "Your Result",
+                style: TextStyle(
+                  fontSize: 50.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Smallcard(
+              colour: Color(0xFF1D1E33),
+              cardChild: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(textResult.toUpperCase(),
+                      style: TextStyle(
+                        color: Color(0xFF24D876),
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+                      )),
+                  Text(
+                    bmiResult,
+                    style: TextStyle(
+                      fontSize: 100.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  Text(
+                    interResult,
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 22.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Calcbuttom(
+              text: "RE-CALCULATE",
+              func: () {
+                Navigator.pop(context);
+              })
+        ],
+      ),
+    );
+  }
+}
